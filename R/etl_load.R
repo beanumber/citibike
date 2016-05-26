450160b6de1aa8196093c3e8702d9c0d48c49ecc
vegahi <vegazhanghi@gmail.com>
2016-05-26 12:35:28 -0400

    Updated load function

--- R/etl_load.R
+++ R/etl_load.R
@@ -10,6 +10,10 @@
 #'   etl_extract() %>%
 #'   etl_transform() %>%
 #'   etl_load()
+#'  library(RSQLite)
+#'  my_db<- src_sqlite( path= bikes$path, create= TRUE)
+#'  my_tbl<- tbl(my_db,"trips")
+#'  head(my_tbl)
 #' 
 #' #' # check the results
 #' \dontrun{
@@ -21,12 +25,18 @@
 #' }
 
 
-etl_load.etl_citibike <- function(obj, year = 2015, month = 1, ...) {
+etl_load.etl_citibike <- function(obj) {
   dir <- attr(obj, "load_dir")
-  src <- sprintf(paste0(dir, "/%04d%02d-citibike-tripdata.csv"), year, month)
-  # write the table to the DB
-  message("Writing flight data to the database...")
-  DBI::dbWriteTable(obj$con, "trips", src, append = TRUE, ...)
+  src <- list.files(dir, full.names = TRUE)
+  push_month<- function(obj,csv,..){
+    # write the table directly to the DB
+    message(paste("Importing bike data from", csv, "to the database..."))
+    if(DBI::dbWriteTable(obj$con, "trips", csv, append= TRUE, ...)) {
+      message( "Data was successfully written to database.")
+    }
+  }
+  sapply(src, push_month, obj = obj, ...)
+  invisible(obj)
 }
 
 
