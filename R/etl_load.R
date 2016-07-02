613e856b4bc59f9f9cf08f6d28fa374a1baabd70
liwencong1995 <liwencong1995@gmail.com>
2016-07-01 20:25:23 -0400

    #6

--- R/etl_load.R
+++ R/etl_load.R
@@ -9,9 +9,9 @@
 #' \dontrun{
 #' bikes <- etl("citibike", dir = "~/Desktop/citibike_data")
 #' bikes %>%
-#'   etl_extract() %>%
-#'   etl_transform() %>%
-#'   etl_load()
+#'   etl_extract(years = 2015, months = 1:2) %>%
+#'   etl_transform(years = 2015, months = 1) %>%
+#'   etl_load(years = 2015, months = 1)
 #' 
 #' trips <- tbl(bikes,"trips")
 #' head(trips)
@@ -28,13 +28,23 @@
 etl_load.etl_citibike <- function(obj, schema = FALSE, years = 2015, months = 1:12, ...) {
   dir <- attr(obj, "load_dir")
   src <- list.files(dir, full.names = TRUE)
-  # write the table to the DB
+  files <- basename(src)
   
+  #valid years and month; create corresponding path
+  year_month <- valid_year_month(years, months) %>%
+    mutate(month = ifelse(month<10, paste0("0",month), month))%>%
+    mutate(year_month = paste0(year, month)) %>%
+    mutate(zip_files = paste0(year_month, "-citibike-tripdata.csv")) %>%
+    filter(zip_files %in% files) %>%
+    mutate(path = paste0(dir,"/",zip_files))
+  path <- year_month$path
+  
+  #Write to Table
   message("Writing flight data to the database...")
-  for (i in src){
-    DBI::dbWriteTable(obj$con, "trips", i, append = TRUE, overwrite = FALSE, ...)
-#                      field.types = c(tripduration = "int", starttime = "text"))
-  }
+  for (i in path){
+    DBI::dbWriteTable(obj$con, "trips", i, 
+                      append = TRUE, overwrite = FALSE, ...)
+    }
   invisible(obj)
 }
 
