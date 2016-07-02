f6ea3f8c4d97cdf8283b573e275c42931a076de8
liwencong1995 <liwencong1995@gmail.com>
2016-07-01 20:08:07 -0400

    fixed a bug in etl_transform. #7

--- R/etl_transform.R
+++ R/etl_transform.R
@@ -22,9 +22,12 @@ etl_transform.etl_citibike <- function(obj, years = 2015, months = 1:12, ...) {
     mutate(month = ifelse(month<10, paste0("0",month), month))%>%
     mutate(year_month = paste0(year, month)) %>%
     mutate(zip_files = paste0(year_month, "-citibike-tripdata.zip")) %>%
-    mutate(zip = ifelse(zip_files %in% files, zip_files, ""))
-  zip <- year_month$zip
+    filter(zip_files %in% files) %>%
+    mutate(path = paste0(dir,"/",zip_files))
+  path <- year_month$path
   new_dir <- attr(obj, "load_dir")
-  for (i in zip) {utils::unzip(i, exdir = new_dir)}
+  for (i in path) {
+    utils::unzip(i, exdir = new_dir)
+    }
   invisible(obj)
 }
